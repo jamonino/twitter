961548984f48738e4470d4ede73e8de227035976
josem <josem@LAPTOP-S1P10O6I>
2021-11-17 13:59:37 +0100

    DB

--- src/main/java/com/albares/twitter/api/addUser.java
+++ src/main/java/com/albares/twitter/api/addUser.java
@@ -2,46 +2,45 @@
 package com.albares.twitter.api;
 
 import com.albares.twitter.db.User;
+import com.albares.twitter.utils.Db;
 import com.albares.twitter.utils.JWTUtils;
-import com.albares.twitter.utils.Parameters;
 import com.albares.twitter.utils.Response;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Path("/addUser")
 public class addUser {
-    
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addUser(User user){   
-        try{
-            Parameters.users.put(Parameters.idUsers.incrementAndGet(),user);
+    public Response addUser(User user) throws SQLException{   
+
             
+            Db myDb = new Db();
+            myDb.connect();
             
+            PreparedStatement ps = myDb.prepareStatement(
+                    "INSERT INTO users (name, pass) VALUES ('?', '?') returning id;"
+            );
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getPass());
+            ResultSet rs = myDb.executeQuery(ps);
             
-            Response r = new Response();
             User u = new User();
-            u.setToken(JWTUtils.generateToken(Parameters.idUsers.get()));
+            while(rs.next()){
+                u.setToken(JWTUtils.generateToken(rs.getInt(1)));
+            }
+            Response r = new Response();
+            
             r.setUser(u);
             r.setResponseCode(1);
             return r;   
-        }catch(Exception e){
-            Response r = new Response();
-            r.setResponseCode(0);
-            return r;
-        }
     }    
-    
-    
 }
-
-
-
-
-
-
 
